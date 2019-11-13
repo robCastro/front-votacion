dea8b4fa067b1b529a48a1df0f97343007e9841c
Katy Cerna <katcerna24@gmail.com>
2019-11-13 11:24:39 -0600

    Avance de Pantalla de Crear Votacion

---
+++ src/app/service/DisenioVotacion/tipo-votacion.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as url from './url_back';
+import  {TipoVotacion} from '../../models/disenio/tipo-votacion';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TipoVotacionService {
+	private httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
+	private urlBase = url.produccion;
+
+  constructor(private http: HttpClient) { }
+
+  public getTipoVotacion(): Observable<TipoVotacion[]>{   //argumento de observable es la clase exportada
+		console.log(this.urlBase);
+		return this.http.get(`${this.urlBase}tipo_votacion`).pipe(
+			map( response => response as TipoVotacion[])
+		);
+	}
+}
---
+++ src/app/service/DisenioVotacion/url_back.ts
@@ -0,0 +1,2 @@
+export const produccion:string = 'http://diseniovotacion.herokuapp.com/api/diseniovotacion/'
+export const desarrollo:string = 'http://localhost:3003/api/diseniovotacion/';
