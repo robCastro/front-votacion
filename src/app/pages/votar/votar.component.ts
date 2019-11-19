bc2f5e252a14c0ae607e862d004f571919cc9fa0
Roberto Castro <roberto.ernesto.castro@gmail.com>
2019-11-19 16:43:44 -0600

    Avance de Desbloquear, desbloqueo aun primitivo usando parametros opcionales de urls

--- src/app/pages/votar/votar.component.ts
+++ src/app/pages/votar/votar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { SocketService } from '../../service/socket.service'
 
 @Component({
   selector: 'app-votar',
@@ -7,9 +11,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotarComponent implements OnInit {
 
-  constructor() { }
+	private id_mesa:number;
 
-  ngOnInit() {
-  }
+	constructor(private socketService: SocketService,
+				private route: ActivatedRoute,
+	) { }
+
+	ngOnInit() {
+		this.displayBloqueado(true);
+		this.route.queryParamMap.subscribe(params =>{
+			this.id_mesa=parseInt(params.get('id_mesa'))
+			console.log('dentro subscribe querymap')
+			console.log(this.id_mesa);
+		})
+		this.socketService.emit('connection',null);
+		this.socketService.listen('desbloquear').subscribe((data:any)=>{
+			console.log(data);
+			console.log("recibido")
+			if(this.id_mesa===data)
+				this.displayBloqueado(false);
+		})
+		console.log('fuera subscribe querymap')
+		console.log(this.id_mesa);	
+	}
+
+	private displayError(err: any){
+		console.log(err);
+		let msg = "";
+		err.error.msg ? msg = err.error.msg : msg = err.message;
+		Swal.fire({
+			title: '<strong>Error!</strong>',
+			html: msg,
+			icon: 'error',
+			confirmButtonText: 'OK'
+		});
+		
+	}
+
+	private displayProcesando(abrir: boolean){
+		if(abrir){
+			Swal.fire({
+				position: 'top-end',
+				title: 'Procesando',
+				html: '...',
+				icon: 'info',
+				showConfirmButton: false,
+				allowOutsideClick: false,
+  				allowEscapeKey: false,
+			});
+		}
+		else{
+			Swal.close();
+		}
+	}
+
+	private displayBloqueado(bloquear: boolean){
+		if(bloquear){
+			Swal.fire({
+				title: 'Bloqueado',
+				html: 'Pasar a mesa',
+				icon: 'warning',
+				showConfirmButton: false,
+				allowOutsideClick: false,
+  				allowEscapeKey: false,
+			});	
+		}
+		else{
+			Swal.close();
+		}
+	}
+
+
 
 }
