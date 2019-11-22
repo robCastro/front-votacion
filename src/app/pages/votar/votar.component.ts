fd2c8c2ab1f1faf609daca6e8cf895afbbe5c61b
Roberto Castro <roberto.ernesto.castro@gmail.com>
2019-11-22 11:15:19 -0600

    uso de ip local para determinar id listo

--- src/app/pages/votar/votar.component.ts
+++ src/app/pages/votar/votar.component.ts
@@ -1,8 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import Swal from 'sweetalert2';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
-import { SocketService } from '../../service/socket.service'
+import { SocketService } from '../../service/socket.service';
+import { MesaService } from '../../service/DisenioVotacion/mesa.service';
+
+
+
+declare global {
+	interface Window {
+		RTCPeerConnection: RTCPeerConnection;
+		mozRTCPeerConnection: RTCPeerConnection;
+		webkitRTCPeerConnection: RTCPeerConnection;
+	}
+}
 
 @Component({
   selector: 'app-votar',
@@ -12,18 +23,27 @@ import { SocketService } from '../../service/socket.service'
 export class VotarComponent implements OnInit {
 
 	private id_mesa:number;
+	private localIp = sessionStorage.getItem('LOCAL_IP');
+	private ipRegex = new RegExp(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/);
+
 
 	constructor(private socketService: SocketService,
 				private route: ActivatedRoute,
+				private zone: NgZone,
+				private mesaService: MesaService,
 	) { }
 
 	ngOnInit() {
 		this.displayBloqueado(true);
-		this.route.queryParamMap.subscribe(params =>{
-			this.id_mesa=parseInt(params.get('id_mesa'))
-			console.log('dentro subscribe querymap')
-			console.log(this.id_mesa);
-		})
+		this.determineLocalIp();
+		this.mesaService.getMesaPorIp(this.localIp).subscribe(
+			mesa => {
+				this.id_mesa = mesa[0].id_mesa;
+				console.log(this.id_mesa);
+			}, err => {
+				this.displayError(err);
+			}
+		);
 		this.socketService.emit('connection',null);
 		this.socketService.listen('desbloquear').subscribe((data:any)=>{
 			console.log(data);
@@ -31,8 +51,6 @@ export class VotarComponent implements OnInit {
 			if(this.id_mesa===data)
 				this.displayBloqueado(false);
 		})
-		console.log('fuera subscribe querymap')
-		console.log(this.id_mesa);	
 	}
 
 	private displayError(err: any){
@@ -82,5 +100,35 @@ export class VotarComponent implements OnInit {
 	}
 
 
+
+
+	private determineLocalIp() {
+		window.RTCPeerConnection = this.getRTCPeerConnection();
+
+		const pc = new RTCPeerConnection({ iceServers: [] });
+		pc.createDataChannel('');
+		pc.createOffer().then(pc.setLocalDescription.bind(pc));
+
+		pc.onicecandidate = (ice) => {
+			this.zone.run(() => {
+				if (!ice || !ice.candidate || !ice.candidate.candidate) {
+					return;
+				}
+
+				this.localIp = this.ipRegex.exec(ice.candidate.candidate)[1];
+				sessionStorage.setItem('LOCAL_IP', this.localIp);
+
+				pc.onicecandidate = () => {};
+				pc.close();
+			});
+		};
+	}
+
+
+	private getRTCPeerConnection() {
+		return window.RTCPeerConnection ||
+		window.mozRTCPeerConnection ||
+		window.webkitRTCPeerConnection;
+	}
 
 }
